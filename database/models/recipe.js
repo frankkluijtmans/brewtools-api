ae9bd0f8600da6036ef4692aa0fc249cfc3a5818
Frank Kluijtmans <frank@studyportals.com>
2018-11-25 20:49:52 +0100

    Finished Recipe model

--- database/models/recipe.js
+++ database/models/recipe.js
@@ -3,12 +3,38 @@ const mongoose = require('mongoose');
 const recipeSchema = new mongoose.Schema({ 
     name: 'string',
     style: 'string',
-    brewery: 'number', 
     collaborators: ['string'],
     og: 'number',
     fg: 'number',
     ibu: 'number',
-    base_volume: 'number'
+    ebc: 'number',
+    base_volume: 'number',
+    boiling_time: 'number',
+    mash_water: 'number',
+    flush_water: 'number',
+    mash: [{
+        temperature: 'number',
+        duration: 'number'
+    }],
+    hops: [{
+        name: 'string',
+        bitterness: 'decimal128',
+        volume: 'number',
+        boiling_time: 'number'
+    }],
+    grains: [{
+        name: 'string',
+        color: 'number',
+        volume: 'number'
+    }],
+    other: [{
+        name: 'string',
+        volume: 'number'
+    }],
+    yeast: {
+        name: 'string',
+        volume: 'number'
+    }
 });
 
 module.exports = mongoose.model('Recipe', recipeSchema);
--- handlers/new-recipe.js
+++ handlers/new-recipe.js
@@ -1,10 +1,76 @@
 const mongoose = require('mongoose');
-const Cat = require('../database/models/cat');
+const Recipe = require('../database/models/recipe');
 
 mongoose.connect(process.env.DATABASE_URL);
 
 module.exports = (req, res) => {
-
-    const kitty = new Cat({ name: req.body.name });
-    kitty.save().then(res.sendStatus(200));
+    console.log(req.body.name);
+    const recipeTest = new Recipe({
+        name: 'Tripple Nipple',
+        style: 'Belgian Triple',
+        collaborators: ['frank_kluyt@hotmail.com'],
+        og: 1057,
+        fg: 1012,
+        ibu: 21,
+        ebc: 20,
+        base_volume: 10,
+        boiling_time: 90,
+        mash_water: 25,
+        flush_water: 10,
+        mash: [
+            {
+                temperature: 60,
+                duration: 15
+            },
+            {
+                temperature: 65,
+                duration: 60
+            },
+            {
+                temperature: 72,
+                duration: 5
+            }
+        ],
+        hops: [
+            {
+                name: 'Centennial',
+                bitterness: 11.2,
+                volume: 45,
+                boiling_time: 90
+            },
+            {
+                name: 'Cascade',
+                bitterness: 8.6,
+                volume: 18,
+                boiling_time: 30
+            }
+        ],
+        grains: [
+            {
+                name: 'Pilsener',
+                color: 8,
+                volume: 600
+            },
+            {
+                name: 'Caramunich',
+                color: 120,
+                volume: 80
+            }
+        ],
+        other: [
+            {
+                name: 'Sweetened orange peel',
+                volume: 6
+            },
+            {
+                name: 'Cilantro',
+                volume: 11
+            }
+        ],
+        yeast: {
+            name: 'Safale US-05',
+            volume: 20
+        }
+    });
+    recipeTest.save().then(res.sendStatus(200));
 };
