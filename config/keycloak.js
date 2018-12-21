626b0d2b6c2fdba049adb91a5bd2a3fe54feeecd
Frank Kluijtmans <frank@studyportals.com>
2018-12-21 22:22:53 +0100

    Added environment configuration

--- config/keycloak.js
+++ config/keycloak.js
@@ -1,7 +1,7 @@
 module.exports = {
-    clientId: 'brewtools-api-dev',
+    clientId: process.env.KEYCLOAK_CLIENT_ID,
     bearerOnly: true,
     serverUrl: 'https://id.brewtools.org/auth',
     realm: 'Brewtools',
-    realmPublicKey: 'MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAgtXSbxs1khYUT87u9mtJn8JQMLEE0MhHwWEnfIT+vhmedHFWv8dYHgE1ItouGFyd+osaWvy/5T2GbvHQY1ClaOD9uDYbvLIbTUOCmDyHkYRqhtwkDE+++vNbBixpIxxe/EfAKiyuxrre8lgBAHbg1XQAR+IEWDPHJruqylkvNpm1ru6nzkPayiMK3+7OcNhYUQNNMDLyOniM8yc5pCUM4FzZmaU75xND2ocjZXnEw6kR29UPQ497sr56CG3G1Sq+2aZquAKEPKE9mNrYmFDrqni38Y9veLQWPdZD0oo4QRrMqhkwLhzOAyD5zkIGoX1ynSximbijgdHTT7AfxWB7ywIDAQAB'
+    realmPublicKey: process.env.KEYCLOAK_REALM_PUBLIC_KEY
 }
