fdb0f683ee9e9dedb748424250aac979b8d93968
Frank Kluijtmans <frank@studyportals.com>
2019-01-06 15:13:34 +0100

    Several fixes for update recipe

--- handlers/recipe/update-recipe.js
+++ handlers/recipe/update-recipe.js
@@ -5,48 +5,82 @@ mongoose.connect(process.env.DATABASE_URL);
 
 module.exports = (req, res) => {
     
-    const updatedRecipe = {
-        name: req.body.name,
-        last_edited: {
-            date: new Date(Date.now())
-        },
-        style: req.body.style,
-        og: req.body.og,
-        fg: req.body.fg,
-        ibu: req.body.ibu,
-        ebc: req.body.ebc,
-        base_volume: parseFloat(req.body.base_volume),
-        boiling_time: req.body.boiling_time,
-        mash_water: parseFloat(req.body.mash_water),
-        flush_water: parseFloat(req.body.flush_water),
-        mash: req.body.mash,
-        hops: req.body.hops.map(hop => {
-            hop.bitterness = parseFloat(hop.bitterness);
-            return hop;
-        }),
-        fermentables: req.body.fermentables,
-        other: req.body.other,
-        yeast: {
-            name: req.body.yeast.name,
-            volume: req.body.yeast.volume
-        }
-    }
+    const token_content = req.kauth.grant.access_token.content;
 
-    const query = Recipe.findOneAndUpdate({ _id: req.params.id }, updatedRecipe);
-
-    query.exec(function (err) {
+    Recipe.findById(req.params.id, (err, doc) => {
 
         if(err) {
 
             res.status(503);
             res.json({
-                error: 'Data mutation error.'
+                error: 'Data retrieval error.'
             });
         }
 
-        res.status(200);
-        res.json({
-            success: true
+        let isCollaborator = false;
+
+        doc.collaborators.forEach(user => {
+            
+            if(user.email === token_content.email 
+                && user.status === 'confirmed') {
+                
+                isCollaborator = true;
+            }
         });
+
+        if(doc.owner.email === token_content.email || isCollaborator) {
+            
+            const updatedRecipe = {
+                name: req.body.name,
+                last_edited: {
+                    date: new Date(Date.now()),
+                    user: token_content.name
+                },
+                style: req.body.style,
+                og: req.body.og,
+                fg: req.body.fg,
+                ibu: req.body.ibu,
+                ebc: req.body.ebc,
+                base_volume: parseFloat(req.body.base_volume),
+                boiling_time: req.body.boiling_time,
+                mash_water: parseFloat(req.body.mash_water),
+                flush_water: parseFloat(req.body.flush_water),
+                mash: req.body.mash,
+                hops: req.body.hops.map(hop => {
+                    hop.bitterness = parseFloat(hop.bitterness);
+                    return hop;
+                }),
+                fermentables: req.body.fermentables,
+                other: req.body.other,
+                yeast: {
+                    name: req.body.yeast.name,
+                    volume: req.body.yeast.volume
+                }
+            }
+        
+            const query = Recipe.findOneAndUpdate({ _id: req.params.id }, updatedRecipe);
+        
+            query.exec(function (err) {
+        
+                if(err) {
+        
+                    res.status(503);
+                    res.json({
+                        error: 'Data mutation error.'
+                    });
+                }
+        
+                res.status(200);
+                res.json({
+                    success: true
+                });
+            });
+        } else {
+
+            res.status(403);
+            res.json({
+                error: 'You are not allowed to do this operation.'
+            });
+        }
     });
 };
